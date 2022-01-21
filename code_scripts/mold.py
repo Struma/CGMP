ca71add2a96268d4c2194d6351ff28e0c5a373f1
struma <struma_dev@protonmail.com>
2022-01-21 00:55:13 -0500

    context to mold.py and gitignore up

--- code_scripts/mold.py
+++ code_scripts/mold.py
@@ -1,8 +1,7 @@
 #/usr/bin/python3
+#tool to make concrete brick molds given a length and thickness of wood.
+#Designed for making cement anchors and monolithes
 import fire
-
-def printer():
-    return print(dang)
 
 def square(length, thick):
     l = (length  - (4 * thick))/4
@@ -13,6 +12,7 @@ def square(length, thick):
          short piece l = {l}, long piece w = {w}\n\
          cut lines: {l}, {ll}, {llw}"
     return print(g)
+
 def rectangle(length, thick):
     l = (length - (4*thick))/3
     w = l/2 + (2*thick)
